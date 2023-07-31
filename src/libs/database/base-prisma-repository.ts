3f230a114a02ea70227d496dd96f938b8a3a8cc9
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-07-31 14:35:34 +0900

    [Feat] moduels 에 Domain post , user 추가

---
+++ src/libs/database/base-prisma-repository.ts
@@ -0,0 +1,6 @@
+import { PrismaClient } from "@prisma/client";
+export class BasePrismaRepository extends PrismaClient {
+    constructor(){
+        super();
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
 import { NestBootStrapApplication } from './bootstrap.application';
-
+const prisma = new PrismaClient().$extend({
+  $model: {
+      $all: {
+          getClass<T extends object>(this: T): new () => T {
+              return class { /**  Implementation */ } as any
+          }
+      },
+  },
+})
 NestBootStrapApplication.getInstance().emit('ready');
