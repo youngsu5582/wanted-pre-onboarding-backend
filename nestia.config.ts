a0da7f65751fdd6285520523602ceb51fa93794f
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-07-31 15:12:37 +0900

    [Feat] Nestia 연동 위한 nestia.config 추가 & schema.prisma 에 정규식 추가

---
+++ nestia.config.ts
@@ -0,0 +1,22 @@
+import { INestiaConfig } from "@nestia/sdk";
+ 
+const config: INestiaConfig = {
+    input: "src/**.controller.ts",
+    swagger: {
+        output: "dist/swagger.json",
+        security: {
+            bearer: {
+                type: "apiKey",
+                name: "Authorization",
+                in: "header",
+            },
+        },
+        servers: [
+            {
+                url: "http://localhost:8000",
+                description: "Local Server"
+            }
+        ],
+    }
+};
+export default config;
