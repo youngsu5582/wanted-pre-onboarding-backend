80003c2017d0fe34278134fed37742019cc2af94
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-08-01 21:00:03 +0900

    [Feat] Domain 을 처리하는 Domain Event 추가

---
+++ src/libs/ddd/entity-base.ts
@@ -0,0 +1,59 @@
+
+  
+  export type AggregateID = string;
+  
+  export interface BaseEntityProps {
+    id: AggregateID;
+    createdAt: Date;
+    updatedAt: Date;
+  }
+  
+  export interface CreateEntityProps<T> {
+    id: AggregateID;
+    props: T;
+    createdAt?: Date;
+    updatedAt?: Date;
+  }
+  
+  export abstract class Entity<EntityProps> {
+    constructor({
+      id,
+      createdAt,
+      updatedAt,
+      props,
+    }: CreateEntityProps<EntityProps>) {
+      this.setId(id);
+      const now = new Date();
+      this._createdAt = createdAt || now;
+      this._updatedAt = updatedAt || now;
+      this.props = props;
+    }
+  
+    protected readonly props: EntityProps;
+
+    protected abstract _id: AggregateID;
+  
+    private readonly _createdAt: Date;
+  
+    private _updatedAt: Date;
+  
+    get id(): AggregateID {
+      return this._id;
+    }
+  
+    private setId(id: AggregateID): void {
+      this._id = id;
+    }
+  
+    get createdAt(): Date {
+      return this._createdAt;
+    }
+  
+    get updatedAt(): Date {
+      return this._updatedAt;
+    }
+  
+    static isEntity(entity: unknown): entity is Entity<unknown> {
+      return entity instanceof Entity;
+    }
+}
