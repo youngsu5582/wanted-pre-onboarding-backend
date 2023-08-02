90cd0f19911bb2d2ef1994eee3da35176cc639d0
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-08-02 15:41:19 +0900

    [Feat] Error 처리 위한 ExceptionBase 추가 & User Domain 에 해당하는 user.errors 추가

---
+++ src/libs/exceptions/exception.base.ts
@@ -0,0 +1,32 @@
+import { RequestContextService } from "../application/context/request-context.service";
+
+export interface SerializedException {
+    message : string;
+    code : string;
+    correlationId : string;
+    stack ?: string;
+    cause ? : string;
+    metadata ?: unknown;
+}
+
+export abstract class ExceptionBase extends Error {
+    abstract code : string;
+    public readonly correlationId : string;
+    constructor(readonly message:string,readonly cause?: Error,readonly metadata ?:unknown){
+        super(message);
+        Error.captureStackTrace(this,this.constructor);
+        const ctx = RequestContextService.getContext();
+        this.correlationId = ctx.requestId;
+    }
+    toJSON() :SerializedException{
+        return {
+            message : this.message,
+            code : this.code,
+            stack : this.stack ,
+            correlationId:this.correlationId,
+            metadata : this.metadata,
+            cause : JSON.stringify(this.cause),
+        }
+
+    }
+}
