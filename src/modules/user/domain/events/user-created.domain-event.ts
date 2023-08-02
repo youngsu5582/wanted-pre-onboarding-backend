bfda907349dad722f87b1d9c9e38b50c77455259
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-08-02 11:13:47 +0900

    [Feat] 차후 도메인 단위 이벤트를 활용하기 위한 UserCreatedDomainEvent 추가

---
+++ src/modules/user/domain/events/user-created.domain-event.ts
@@ -0,0 +1,9 @@
+import { DomainEvent, DomainEventProps } from "@src/libs/ddd/domain-event-base";
+
+export class UserCreatedDomainEvent extends DomainEvent {
+    readonly email : string;
+    constructor(props : DomainEventProps<UserCreatedDomainEvent>){
+        super(props);
+        this.email = props.email;
+    }
+}
--- src/modules/user/domain/user.entity.ts
+++ src/modules/user/domain/user.entity.ts
@@ -1,6 +1,7 @@
 import { randomId } from "@src/utils/random-id";
 import { CreateUserProps, UserProps } from "./user.types";
 import { AggregateId, AggregateRoot } from "@src/libs/ddd/aggregate-root-base";
+import { UserCreatedDomainEvent } from "./events/user-created.domain-event";
 
 
 
@@ -8,11 +9,9 @@ export class UserEntity extends AggregateRoot<UserProps>{
     protected readonly _id : AggregateId;
     static create(create : CreateUserProps) : UserEntity {
         const id = randomId();
-        const now = new Date();
         const props : UserProps = {...create};
-        
         const user = new UserEntity({id,props});
-        user.createdAt
+        user.addEvent(new UserCreatedDomainEvent({aggregatedId:id,email:props.email}));
         return user;
     } 
 }
